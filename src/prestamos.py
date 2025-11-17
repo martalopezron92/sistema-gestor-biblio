6a88779f9459babba6e7c7ebd54e0ba1bfa05fe6
Marta92 <mlopron829@g.educaand.es>
2025-11-17 18:09:55 +0100

    Añado funcionalidad prestamos

---
+++ src/prestamos.py
@@ -0,0 +1,20 @@
+def prestamos(libro, usuario, fecha_prestamo, fecha_devolucion):
+    """
+    Crea un diccionario que representa un préstamo de libro con el libro prestado, el usuario,
+    la fecha de préstamo y la fecha de devolución.
+
+    Parámetros:
+    libro (dict): Un diccionario que representa el libro prestado.
+    usuario (str): El nombre del usuario que realiza el préstamo.
+    fecha_prestamo (str): La fecha en que se realizó el préstamo.
+    fecha_devolucion (str): La fecha en que se debe devolver el libro.
+
+    Retorna:
+    dict: Un diccionario con las claves 'libro', 'usuario', 'fecha_prestamo' y 'fecha_devolucion'.
+    """
+    return {
+        'libro': libro,
+        'usuario': usuario,
+        'fecha_prestamo': fecha_prestamo,
+        'fecha_devolucion': fecha_devolucion
+    }
